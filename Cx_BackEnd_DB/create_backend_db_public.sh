1d6e3d3d28254033bdcc1e7277be3f013b57a1e1
Vladimir Stojanovic <vladimir.stojanovic@keylight.rs>
2020-05-15 14:59:21 +0200

    Optional SSH actions. Just switch to new tenant

--- Cx_BackEnd_DB/create_backend_db_public.sh
+++ Cx_BackEnd_DB/create_backend_db_public.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
 expectedEnvFileName=".env";
+sedKeyWordRegEx='\(\(TENANT_UUID\)\|\(TENANT\)\|\(MYSQL_NAME\)\)=';
+grepKeyWordRegEx='((TENANT_UUID)|(TENANT)|(MYSQL_NAME))=';
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-if (( $# < 6 )); then
+if (( $# < 1 )); then
 	echo "Missing arguments"
-	echo "$0 CUSTOMER_NAME ENV_CONFIG_PATH SSH_TARGET SSH_TUNNEL_MYSQL_HOST SSH_TUNNEL_REDIRECT_PORT SSH_MYSQL_USER"
+	echo "$0 \$1:CUSTOMER_NAME [\$2:ENV_CONFIG_PATH=$expectedEnvFileName] [\$3:SSH_TARGET \$4:SSH_TUNNEL_MYSQL_HOST \$5:SSH_TUNNEL_REDIRECT_PORT \$6:SSH_MYSQL_USER]"
 	exit 1
 fi
 
 # Instructions for SandBox DB access: https://www.notion.so/keylight/Infrastructure-0068f50a574e4b5bbb5f2314ca73e125
 tenantName=$1
 envConfigFilePath=$2
-sshTarget=$3
-sshTunnelMySqlHost=$4
-sshTunnelRedirectPort=$5
-sshMySqlUser=$6
+if [ -z "$envConfigFilePath" ]; then
+	envConfigFilePath=$expectedEnvFileName
+fi
 
 if [[ "$envConfigFilePath" != */"$expectedEnvFileName"  ]] && [[ "$envConfigFilePath" != "$expectedEnvFileName" ]]; then
 	echo "Invalid config file path [$envConfigFilePath]."
@@ -33,48 +34,48 @@ fi
 # tenantDb=$(echo $tenantName"_DB" | tr - _)
 tenantDb=$tenantName
 
-sedKeyWordRegEx='\(\(TENANT_UUID\)\|\(TENANT\)\|\(MYSQL_NAME\)\)=';
-grepKeyWordRegEx='((TENANT_UUID)|(TENANT)|(MYSQL_NAME))=';
-envTenantLinesToCheck=( "TENANT=$tenantName" "MYSQL_NAME=$tenantDb" )
-
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
-for l in "${envTenantLinesToCheck[@]}"; do
-	existing_tenant_field=$(egrep $l"$" $envConfigFilePath)
-	if [ "$existing_tenant_field" != "" ]; then
-		echo "Found existing tenant field [$existing_tenant_field]"
-		echo "in [$envConfigFilePath]"
-		echo "Please remove related config fields for customer [$tenantName]"
-		exit 1
-	fi
-done
 
-sqlDumpFolder=$(dirname "$0")/Dumps
-mkdir -p $sqlDumpFolder
-tenantSqlDumpFile=$sqlDumpFolder/"$tenantDb"_$(date +'%Y-%m-%d').sql
-
-if [ -f "$tenantSqlDumpFile" ] && [ -s "$tenantSqlDumpFile" ]; then
-    echo "Cx DB [$tenantDb] already exported @ [$tenantSqlDumpFile]"
+if [ -z "$3" ] || [ -z "$4" ] || [ -z "$5" ] || [ -z "$6" ]; then
+	echo "No SSH parameters provided. Skipping (re)creation of local DB [$tenantDb]"
 else
-	# Setup SSH tunnel for querying SandBox DB
-	# For it to work you have to add your private SSH key @ https://github.com/keylightberlin/key-people
-	echo "Setting up SSH tunnel for SandBox DB access (exit the new terminal afterwards)"	
-	gnome-terminal -- ssh -L $sshTunnelRedirectPort:$sshTunnelMySqlHost $sshTarget &
-	# Export (dump) the requested Cx DB
-	echo "Exporting SandBox DB [$tenantDb] to file [$tenantSqlDumpFile]..."	
-	mysqldump $tenantDb -u $sshMySqlUser -p -h 127.0.0.1 -P $sshTunnelRedirectPort > $tenantSqlDumpFile || exit 1
+	sqlDumpFolder=$(dirname "$0")/Dumps
+	mkdir -p $sqlDumpFolder
+	tenantSqlDumpFile=$sqlDumpFolder/"$tenantDb"_$(date +'%Y-%m-%d').sql
+
+	if [ -f "$tenantSqlDumpFile" ] && [ -s "$tenantSqlDumpFile" ]; then
+		echo "Cx DB [$tenantDb] already exported @ [$tenantSqlDumpFile]"
+	else
+		sshTarget=$3
+		sshTunnelMySqlHost=$4
+		sshTunnelRedirectPort=$5
+		sshMySqlUser=$6
+		
+		# Setup SSH tunnel for querying SandBox DB
+		# For it to work you have to add your private SSH key @ https://github.com/keylightberlin/key-people
+		echo "Setting up SSH tunnel for SandBox DB access (exit the new terminal afterwards)"	
+		gnome-terminal -- ssh -L $sshTunnelRedirectPort:$sshTunnelMySqlHost $sshTarget &
+		# Export (dump) the requested Cx DB
+		echo "Exporting SandBox DB [$tenantDb] to file [$tenantSqlDumpFile]..."	
+		mysqldump $tenantDb -u $sshMySqlUser -p -h 127.0.0.1 -P $sshTunnelRedirectPort > $tenantSqlDumpFile || exit 1
+
+		echo "Killing SSH tunnel process"
+		pkill -f "ssh.*$sshTunnelMySqlHost"
+	fi
+
+	# sql_user=$(grep -oP "(?<=MYSQL_USER=).*" $envConfigFilePath)
+	# sql_pass=$(grep -oP "(?<=MYSQL_PASS=).*" $envConfigFilePath)
+
+	# (Re)creating tenant DB via MySQL commands
+	sudo mysql -u root -e "drop database if exists \`$tenantDb\`; create database \`$tenantDb\`" || exit 1
+	# Import the Cx DB locally
+	echo "Importing [$tenantDb] DB from file [$tenantSqlDumpFile]"
+	sudo mysql -u root $tenantDb < $tenantSqlDumpFile || exit 1
 fi
 
-# sql_user=$(grep -oP "(?<=MYSQL_USER=).*" $envConfigFilePath)
-# sql_pass=$(grep -oP "(?<=MYSQL_PASS=).*" $envConfigFilePath)
-
-# (Re)creating tenant DB via MySQL commands
-sudo mysql -u root -e "drop database if exists \`$tenantDb\`; create database \`$tenantDb\`" || exit 1
-# Import the Cx DB locally
-echo "Importing [$tenantDb] DB from file [$tenantSqlDumpFile]"
-sudo mysql -u root $tenantDb < $tenantSqlDumpFile || exit 1
 tenantUUID=$(sudo mysql -u root $tenantDb -se 'select uuid from tenant limit 1')
-if [ "$tenantUUID" == "" ]; then
+if [ -z "$tenantUUID" ]; then
 	echo "Tenant UUID not obtained from local DB [$tenantDb]"
 	exit 1
 fi
@@ -84,10 +85,7 @@ if (( $tenantCount != 1 )); then
 	echo "Found $tenantCount tenants, using first UUID [$tenantUUID]"
 fi
 
-envTenantLinesToAdd=( "TENANT_UUID=$tenantUUID" "${envTenantLinesToCheck[@]}" )
-
-# Comment out existing tenant settings, create a .bak backup file.
-sed -i.bak 's/\(^'$sedKeyWordRegEx'\)/#\ \1/' "$envConfigFilePath"
+envTenantLinesToAdd=( "TENANT_UUID=$tenantUUID" "TENANT=$tenantName" "MYSQL_NAME=$tenantDb" )
 
 # Find a starting line for tenant settings to insert the new tenant settings at that position.
 # Otherwise fallback to the start of file.
@@ -96,16 +94,13 @@ if [ "$starting_line" = "" ]; then
 	starting_line=1
 fi
 
+# Remove existing tenant settings, create a .bak backup file.
+sed -i.bak '/^[# \t]*'$sedKeyWordRegEx'/d' "$envConfigFilePath"
+
 echo "Adding [$tenantName] configuration @ line #$starting_line of [$envConfigFilePath]:"
-# Input new line at the given position
-envInput=$starting_line"i"'\\'
-sed -i $envInput "$envConfigFilePath"
 # Input new tenant settings at the given position
 for l in "${envTenantLinesToAdd[@]}"; do
 	echo "    [$l]"
 	envInput=$starting_line"i"$l
 	sed -i "$envInput" "$envConfigFilePath"
 done
-
-echo "Killing SSH tunnel process"
-pkill -f "ssh.*$sshTunnelMySqlHost"
