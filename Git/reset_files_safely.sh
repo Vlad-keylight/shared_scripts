65705f7d9e38bb7e6f7297da19d6abe5aaabbe2c
Vladimir Stojanovic <vladimir.stojanovic@keylight.rs>
2020-06-02 17:59:59 +0200

    Target origin

--- Git/reset_files_safely.sh
+++ Git/reset_files_safely.sh
@@ -12,7 +12,7 @@ then
 	ScriptFailure "Unable to get the current git branch"
 fi
 
-diffFiles=$(git diff master --name-only)
+diffFiles=$(git diff origin/master --name-only)
 diffFilesCount=$(echo "$diffFiles" | grep -c '')
 if (( "$diffFilesCount" == 0 )) || [ -z "$diffFiles" ]; then
     LogSuccess "No diff files found on branch [$gitInitialBranch]"
