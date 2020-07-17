159e22fcefc2cd1d5b85ae2f4d6dbb3422cf05e0
Vladimir Stojanovic <vladimir.stojanovic@keylight.rs>
2020-07-17 14:48:57 +0200

    Branch name parameter

--- Git/create_rebased_pull_request.sh
+++ Git/create_rebased_pull_request.sh
@@ -2,10 +2,14 @@ currentScriptFolderName=$(dirname "$0")
 # Include common helper functions
 . "$currentScriptFolderName/_git_common.sh" --source-only
 
-gitCurrentBranch=$(GetCurrentBranchName)
-LogSuccess "Current Git branch [$gitCurrentBranch]"
-rebasedBranchName="${gitCurrentBranch}-rebased"
+rebasedBranchName=$(GetBranchFullName $1)
+if [ -z "$rebasedBranchName" ]; then
+	gitCurrentBranch=$(GetCurrentBranchName)
+	LogSuccess "Current Git branch [$gitCurrentBranch]"
+	rebasedBranchName="${gitCurrentBranch}-rebased"
+fi
+
 if [ -n "$(ConfirmAction Rebase to new branch [$rebasedBranchName] and push from it)" ]; then
-    RunGitCommandSafely "git checkout -b $rebasedBranchName"
-    RunGitCommandSafely "git push -u origin $rebasedBranchName"
+	RunGitCommandSafely "git checkout -b $rebasedBranchName"
+	RunGitCommandSafely "git push -u origin $rebasedBranchName"
 fi
